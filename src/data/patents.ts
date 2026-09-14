0a8047495062ddf365942151902cfc7b73361da3
aditya-bhattacharyya <dr.aditya.bhattacharyya@gmail.com>
2026-09-14 18:38:59 +0530

    Add patents data to patents.ts

---
+++ src/data/patents.ts
@@ -0,0 +1,16 @@
+export const patents = [
+  {
+    title: 'Process for the Synthesis of 3-Spiropiperidino Indolenines',
+    inventors: 'S. Pradhan, C. K. Shahi, A. Bhattacharyya, M. K. Ghorai',
+    patent: 'Indian Patent Application IN 201811022519',
+    status: 'Granted',
+    year: '2019',
+  },
+  {
+    title: 'Process for the Synthesis of Quinoxalines',
+    inventors: 'C. K. Shahi, S. Pradhan, A. Bhattacharyya, M. K. Ghorai',
+    patent: 'Indian Patent Application IN 201711016754',
+    status: 'Granted',
+    year: '2019',
+  },
+];
